4e8a29174e3e91df7a63eb09d3023f94855e1b15
anas <anas3506606@gmail.com>
2020-05-27 13:00:55 +0300

    Updates

---
+++ precache-manifest.8f332abbbeab9ad147f854dab0f956f2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae7ec39bc4a11912bf7253c7cd7d214b",
+    "url": "/reactn/index.html"
+  },
+  {
+    "revision": "5e3bab91dcda68d45d0b",
+    "url": "/reactn/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "dc62344ab96cca8dec8b",
+    "url": "/reactn/static/js/2.1359024e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactn/static/js/2.1359024e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e3bab91dcda68d45d0b",
+    "url": "/reactn/static/js/main.76253034.chunk.js"
+  },
+  {
+    "revision": "90eeae3cece14c297c5b",
+    "url": "/reactn/static/js/runtime-main.d71750f6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reactn/static/media/logo.5d5d9eef.svg"
+  }
+]);
